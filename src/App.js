98620e0c0c15f4cbd42cf67f51f304d49db6bb8f
dino9611 <aldinorahman36@gmail.com>
2021-02-24 15:16:52 +0700

    Update data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import "./App.css";
 import { Table } from "reactstrap";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 // * crud didalam state
 // * karena pake yarn npm install itu sama dengan yarn add npm install == yarn add
 
@@ -24,6 +26,12 @@ class App extends Component {
     emailInp: "",
     roleInp: "",
     indexdelete: -1,
+    indexEdit: -1,
+    EditData: {
+      username: "",
+      email: "",
+      role: "",
+    },
   };
 
   // renderUsers = this.state.users.map((val, index) => {
@@ -42,9 +50,10 @@ class App extends Component {
 
   // * boleh cara yang seperti dibawah ini untuk merender data
   // * digunakan pada saat membutuhkan logic
+
   renderUsers = () => {
     return this.state.users.map((val, index) => {
-      if (index == this.state.indexdelete) {
+      if (index === this.state.indexdelete) {
         return (
           <tr key={index}>
             <td>{index + 1}</td>
@@ -64,6 +73,56 @@ class App extends Component {
             </td>
           </tr>
         );
+      } else if (index === this.state.indexEdit) {
+        return (
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>
+              <input
+                name="username"
+                type="text"
+                placeholder="username"
+                className="form-control"
+                value={this.state.EditData.username}
+                onChange={this.onInputEditChage}
+              />
+            </td>
+            <td>
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+                className="form-control"
+                value={this.state.EditData.email}
+                onChange={this.onInputEditChage}
+              />
+            </td>
+            <td>
+              <select
+                name="role"
+                onChange={this.onInputEditChage}
+                value={this.state.EditData.role}
+                className="form-control"
+              >
+                {this.renderRole()}
+              </select>
+            </td>
+            <td>
+              <button
+                className="btn btn-success mx-2"
+                onClick={this.onSaveEditClick}
+              >
+                Save
+              </button>
+              <button
+                className="btn btn-primary mx-2"
+                onClick={this.onCancelEditClick}
+              >
+                Cancel
+              </button>
+            </td>
+          </tr>
+        );
       }
       return (
         <tr key={index}>
@@ -72,7 +131,12 @@ class App extends Component {
           <td>{val.email}</td>
           <td>{val.role}</td>
           <td>
-            <button className="btn btn-primary mx-2">Edit</button>
+            <button
+              className="btn btn-primary mx-2"
+              onClick={() => this.onEditClick(index)}
+            >
+              Edit
+            </button>
             <button
               className="btn btn-danger mx-2"
               onClick={() => this.onDeleteClick(index)}
@@ -112,7 +176,11 @@ class App extends Component {
   // * cara mendapatkan input ada 2 metode :
   // * 1. dengan menggunakan onchange
   // * 2. dengan ref class pake createRef
-
+  onInputEditChage = (event) => {
+    let Editdata = this.state.EditData;
+    Editdata[event.target.name] = event.target.value;
+    this.setState({ EditData: Editdata });
+  };
   onAddClick = () => {
     // const role = this.state.roleInp;
     // const username = this.state.usernameInp;
@@ -134,7 +202,15 @@ class App extends Component {
         emailInp: "",
       });
     } else {
-      alert("harus diisi");
+      toast.error("input harus diisi bro", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
     }
   };
 
@@ -151,6 +227,85 @@ class App extends Component {
     this.setState({ users: usersData, indexdelete: -1 });
   };
 
+  onEditClick = (index) => {
+    let EditData = this.state.EditData;
+    let users = this.state.users;
+    EditData = {
+      ...EditData,
+      username: users[index].username,
+      email: users[index].email,
+      role: users[index].role,
+    };
+    this.setState({ indexEdit: index, EditData: EditData });
+  };
+
+  onCancelEditClick = () => {
+    this.setState({
+      EditData: {
+        username: "",
+        email: "",
+        role: "",
+      },
+      indexEdit: -1,
+    });
+  };
+
+  onSaveEditClick = () => {
+    // * dengan destructuring
+    // const { EditData, users, indexEdit } = this.state;
+    // const { username, email, role } = EditData;
+    // !tanpa destructring
+    let username = this.state.EditData.username;
+    let email = this.state.EditData.email;
+    let role = this.state.EditData.role;
+    let users = this.state.users;
+    let indexEdit = this.state.indexEdit;
+    // *======================= *//
+    if (username && email && role) {
+      let data = {
+        username: username,
+        email: email,
+        role: role,
+      };
+      let usersdata = users;
+      usersdata.splice(indexEdit, 1, data);
+      this.setState({
+        users: usersdata,
+        indexEdit: -1,
+      });
+
+      toast.success("berhasil edit ", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      this.setState({
+        EditData: {
+          username: "",
+          email: "",
+          role: "",
+        },
+        indexEdit: -1,
+      });
+      toast.error("Gak jadi Edit", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+
+  // * input buat edit
+  // * nanti displice seperti biasanya
   render() {
     return (
       <div>
@@ -213,6 +368,7 @@ class App extends Component {
             </tbody>
           </Table>
         </div>
+        <ToastContainer />
       </div>
     );
   }
